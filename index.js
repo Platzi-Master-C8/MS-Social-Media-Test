c3d62ef9b0b511c954413ffd0b52294fa7507548
CARLOS ANDRES SANCHEZ GONZALEZ <casan3@gmail.com>
2021-12-09 19:50:38 -0600

    Add facebook login endpoint

--- index.js
+++ index.js
@@ -2,17 +2,59 @@
 
 const {
   ApolloServer,
-  ApolloServerPluginStopHapiServer
+  ApolloServerPluginStopHapiServer,
 } = require('apollo-server-hapi');
 
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
 
+const Bell = require('@hapi/bell');
 const Hapi = require('@hapi/hapi');
 
 const init = async (typeDefs, resolvers) => {
-
   const app = Hapi.server({ port: 4000 });
+  await app.register(Bell);
+
+  app.auth.strategy('facebook', 'bell', {
+    provider: 'facebook',
+    password: 'cookie_encryption_password_secure',
+    isSecure: false,
+    clientId: '1555047951509441',
+    clientSecret: 'd73fd234a77f38ca54331a9ad440c786',
+    location: app.info.uri
+  });
+
+  app.route([{
+    method: ['GET', 'POST'], // Must handle both GET and POST
+    path: '/loginFacebook', // The callback endpoint registered with the provider
+    options: {
+      auth: {
+        mode: 'try',
+        strategy: 'facebook',
+      },
+      handler: (request, h) => {
+        if (!request.auth.isAuthenticated) {
+          return `Authentication failed due to: ${request.auth.error.message}`;
+        }
+
+        console.log(request.auth.credentials);
+
+        // Perform any account lookup or registration, setup local session,
+        // and redirect to the application. The third-party credentials are
+        // stored in request.auth.credentials. Any query parameters from
+        // the initial request are passed back via request.auth.credentials.query.
+
+        return h.redirect('/home');
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/home',
+    handler: (request, h) => {
+      return {msg: 'hi'}
+    }
+  }]);
 
   const server = new ApolloServer({
     typeDefs,
