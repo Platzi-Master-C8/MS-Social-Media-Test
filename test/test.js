84f8f4a14a1ca48077dbe927c6be303b56a943db
IngDiegoC12 <indfcr12@hotmail.com>
2022-01-22 08:50:51 -0500

    Adig user router test!

--- test/test.js
+++ test/test.js
@@ -2,18 +2,19 @@ const Lab = require('@hapi/lab');
 const lab = exports.lab = Lab.script(); //export test script
 const Code = require('@hapi/code');
 const server = require("../index");
+const pool = require('../libs/postgres.pool');
 
 lab.experiment("Basic HTTP Tests", function() {
 	// tests
-	lab.test("GET /{yourname*} (endpoint test)", async function() {
+	lab.test("GET /api/v1/users/{id ?} (endpoint test)", async function() {
 		const options = {
 			method: "GET",
-			url: "/hi"
+			url: "/api/v1/users/3"
 		};
 		// server.inject lets you simulate an http request
 		const response = await server.inject(options);
 		Code.expect(response.statusCode).to.equal(200);  //  Expect http response status code to be 200 ("Ok")
-		Code.expect(response.result).to.have.length(5); // Expect result to be "Hello Timmy!" (12 chars long)
+		Code.expect(response.result).to.have.object; // Expect result to be "Hello Timmy!" (12 chars long)
 		await server.stop();
 	});
 });
